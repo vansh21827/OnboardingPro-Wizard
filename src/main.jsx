9b8e682d202f25e11e01cc6240f92e3263483443
vansh21827 <vansh21827@gmail.com>
2026-07-12 10:31:54 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "styles/globals.css";
-import "styles/layout.css";
-import "styles/components.css";
-import "styles/wizard.css";
+import "../styles/globals.css";
+import "../styles/layout.css";
+import "../styles/components.css";
+import "../styles/wizard.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 <React.StrictMode>
