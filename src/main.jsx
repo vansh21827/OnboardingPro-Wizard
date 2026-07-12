c615b2779836930bcef4ffdb535cc1cc3a928233
vansh21827 <vansh21827@gmail.com>
2026-07-12 10:25:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./src/App.jsx";
+import App from "./App";
 
 import "styles/globals.css";
 import "styles/layout.css";
